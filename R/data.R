d37276157c94825b1e211dc4a02a32f1931eccf9
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2023-10-20 10:44:18 +0200

    Add new dataset `paternity`

--- R/data.R
+++ R/data.R
@@ -5,3 +5,18 @@
 #' @format A data frame with 18 rows and 5 columns.
 #'
 "LINKAGEMAP"
+
+
+#' Dataset for a paternity case
+#'
+#' A list of two pedigrees forming the hypotheses in a paternity case: H1 (AF is
+#' the father of CH) and H2 (unrelated). AF and CH are genotyped with 11 marker.
+#'
+#' @format A list of two pedigrees, named H1 and H2.
+#'
+#' @examples
+#' library(forrel)
+#'
+#' plotPedList(paternity, marker = "SE33")
+#' kinshipLR(paternity)
+"paternity"
---
+++ data-raw/paternity.R
@@ -0,0 +1,23 @@
+## code to prepare `paternity` dataset goes here
+library(pedsuite)
+library(ibdsim2)
+
+# Markers in builtin LINKAGEMAP also appearing in NorwegianFrequencies
+mmap = subset(LINKAGEMAP, Marker %in% names(NorwegianFrequencies))
+db = NorwegianFrequencies[mmap$Marker]
+ids = c("AF", "CH")
+
+# True pedigree: Father & son
+ped1 = nuclearPed(fa = "AF", ch = "CH") |>
+  profileSim(ids = ids, markers = db, seed = 1729) |>
+  setMutmod(model = "stepwise", rate = 0.002, range = 0.1, rate2 = 1e-6)
+
+# Add mutation in first marker (12/15 -> 12/14)
+ped1 = setGenotype(ped1, marker = 1, ids = "CH", geno = "12/14")
+
+# Unrelated hypothesis
+ped2 = singletons(ids) |>
+  transferMarkers(from = ped1, to = _)
+
+paternity = list(H1 = ped1, H2 = ped2)
+usethis::use_data(paternity, overwrite = TRUE)
