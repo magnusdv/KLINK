b516970a262307e09510eaa7d5615c3afdd42e47
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2023-11-06 18:03:46 +0100

    Implement special autoscaling

--- R/plotPed.R
+++ R/plotPed.R
@@ -1,5 +1,5 @@
 
-plotPed = function(x, marker = NULL, ...) {
+plotPed = function(x, cex = 1.2, marker = NULL, ...) {
   x = foldLabs(x)
 
   if(!is.null(marker) && marker == "")
@@ -9,7 +9,26 @@ plotPed = function(x, marker = NULL, ...) {
   miss = grep(":missing:", allLabs, value = TRUE, fixed = TRUE)
   nonmiss = setdiff(allLabs, miss)
 
-  plot(x, marker = marker, hatched = typedMembers, frames = FALSE, labs = nonmiss,
-       col = list(red = miss), lwd = list("2" = miss), lty = list(dashed = miss),
-       autoScale = TRUE, minsize = 0.15, ...)
+  iter = 0
+  tryAgain = TRUE
+
+  while(tryAgain) {
+    iter = iter + 1
+    if(iter > 1)
+      cex = cex * 0.9
+
+    tryCatch({
+      plot(x, marker = marker, hatched = typedMembers, frames = FALSE, labs = nonmiss,
+           col = list(red = miss), lwd = list("2" = miss), lty = list(dashed = miss),
+           cex = cex, autoScale = TRUE, minsize = 0.15, ...)
+      tryAgain = FALSE
+    },
+    error = function(e) {
+      msg = conditionMessage(e)
+      if(!grepl("reduce cex", msg))
+        stop(msg, call. = FALSE)
+      else if(iter == 10)
+        stop("Autoscale fail: Pedigree too large for plot window.\n\n(This does not affect the LR calculations.)", call. = FALSE)
+    })
+  }
 }
