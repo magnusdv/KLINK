95ddabc75d8ec419a409d04b86b7e5e18cab05ce
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2024-06-26 13:54:30 +0200

    Reimplement karyogram plot

--- R/karyo.R
+++ R/karyo.R
@@ -1,5 +1,5 @@
-#' @importFrom graphics legend par plot.new points rect text.default
-karyogram = function(markerdata, cols = KARYOPALETTE, bgcol = "gray92") {
+#' @importFrom graphics par plot.new points rect segments text.default
+karyogram = function(linkageMap, linkedPairs = NULL, bgcol = "gray92") {
 
   h = 0.7
   plot.new()
@@ -7,24 +7,41 @@ karyogram = function(markerdata, cols = KARYOPALETTE, bgcol = "gray92") {
   oldpar = par(no.readonly = TRUE)
   on.exit(par(oldpar))
 
-  par(mar = c(1,2,1,1), usr = c(0, max(CHROM.MB), 22+h, 1), xpd = TRUE)
+  par(mar = c(1,1.5,1,1), usr = c(0, max(CHROM.MB), 22+h, 1), xpd = TRUE)
 
   rect(xleft = 0, ybottom = 1:22, xright = CHROM.MB, ytop = 1:22 + h, col = bgcol)
   text.default(0, 1:22 + h/2, labels = 1:22, pos = 2)
 
-  # Positions
-  chr = markerdata$Chrom
-  x = markerdata$PosCM * CHROM.MB[chr]/CHROM.CM[chr]
-  y = chr + h/2
-  points(x, y, bg = cols[markerdata$Pair], pch = 21, cex = 2.2)
+  # Linked pairs
+  m = linkageMap$Marker
+  if(is.null(linkedPairs))
+    linkedPairs = getLinkedPairs(m, linkageMap)
 
-  legend(max(CHROM.MB), 13+h/2, xjust = 1, y.intersp = 0, legend = 1:9,
-         pch = 21, pt.cex = 2.2, pt.bg = cols[1:9], bty = "n")
+  pp = lp2vec(m, linkedPairs)
+  islinked = !is.na(pp)
+  pair1 = islinked & !duplicated(pp)
+  pair2 = islinked & duplicated(pp)
+
+  # Positions
+  chr = linkageMap$Chrom
+  y = chr + h/2
+  x = pmin(linkageMap$PosCM/CHROM.CM[chr], 1) * CHROM.MB[chr]  # cm -> mb
+
+  # Segments first, to hide endpoints
+  segments(x0 = x[pair1], x1 = x[pair2], y0 = y[pair1], y1 = y[pair2], col = 1, lwd = 1.3)
+
+  points(x[islinked], y[islinked], bg = KARYOPALETTE[pp[islinked]], pch = 21, cex = 2.5)
+  points(x[!islinked], y[!islinked], col = 1, pch = 21, cex = 2.5)
+
+  # Labels inside points: White text on dark colours
+  bw = rep(1, length(pp))
+  bw[islinked] = DARKLIGHT[pp[islinked]]
+  text.default(x, y, labels = seq_along(m), cex = 0.8, col = c("black", "white")[bw])
 }
 
 
 
-# Hardcoded parameters ----------------------------------------------------
+# Hard-coded parameters ----------------------------------------------------
 
 
 CHROM.MB = c(247.2, 242.9, 199.5, 191.3, 180.9, 170.9, 158.8, 146.3, 140.3,
@@ -39,3 +56,8 @@ KARYOPALETTE = c("#FD3216FF", "#00FE35FF", "#6A76FCFF", "#FED4C4FF", "#FE00CEFF"
                  "#F6F926FF", "#FF9616FF", "#479B55FF", "#EEA6FBFF", "#DC587DFF", "#D626FFFF",
                  "#6E899CFF", "#00B5F7FF", "#B68E00FF", "#C9FBE5FF", "#FF0092FF", "#22FFA7FF",
                  "#E3EE9EFF", "#86CE00FF", "#BC7196FF", "#7E7DCDFF", "#FC6955FF", "#E48F72FF")
+
+DARKLIGHT = c(2,1,2,1,2,1,
+              1,1,2,1,2,2,
+              2,1,2,1,2,1,
+              1,1,2,2,1,2)
