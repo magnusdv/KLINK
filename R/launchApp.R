e264944b3fa97671a0d0bd0dc5d42585f1219589
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2024-08-16 16:08:08 +0200

    Add `version` argument to `runKLINK()`

--- R/launchApp.R
+++ R/launchApp.R
@@ -1,6 +1,10 @@
 #' Launch KLINK
 #'
-#' This launches the KLINK app. `runKLINK()` is a synonym for `launchApp()`.
+#' This launches the KLINK app. `runKLINK()` is a synonym for `launchApp()`, but
+#' with an additional argument `version`.
+#'
+#' @param version A character, e.g. "1.0.0". If the installed version of KLINK
+#'   differs from this, the program aborts with an error.
 #'
 #' @return No return value, called for side effects.
 #'
@@ -17,4 +21,17 @@ launchApp = function() {
 
 #' @export
 #' @rdname launchApp
-runKLINK = launchApp
+runKLINK = function(version) {
+  supplied = as.package_version(version)
+  inst = utils::packageVersion("KLINK")
+  if(inst != supplied) {
+    msg = c("",
+      sprintf("The supplied version number (%s) differs from the installed version (%s)", supplied, inst), "",
+      sprintf("To install KLINK v%s, try restarting R and running this command:", supplied),
+      sprintf('> remotes::install_version("KLINK", version = "%s")', supplied)
+    )
+    stop2(paste0(msg, collapse = "\n"))
+  }
+
+  shiny::runApp(system.file("shiny", package = "KLINK"))
+}
