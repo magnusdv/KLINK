ac26e4076e96f484c47778e719dcffe9a09e02f6
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2025-05-07 14:22:58 +0200

    New function `getTotals()`; simplify `addTotals()`

--- R/addTotals.R
+++ R/addTotals.R
@@ -1,37 +1,42 @@
-#' Add totals to a data frame with LR results
+#' Get LR totals
 #'
-#' Adds a row with totals for the specified columns, formed by multiplying the
-#' values in each column.
+#' Find, or add, the LR totals in a data frame with marker-wise LR values. The
+#' totals are found by multiplying the values in each column, after removing
+#' `NA`'s.
 #'
 #' Note that for the multiplication to respect linked markers, the input table
-#' should include LR results for only one marker in each linkage group. This is
-#' the the case e.g. if the input was produced by [linkedLR()].
+#' should include LR values for only one marker in each linkage group, and
+#' `NA`'s elsewhere. This is the format used by [linkedLR()].
 #'
-#' @param x A data frame with LR results.
+#' @param x A data frame with LR results, typically the output of [linkedLR()].
 #' @param cols A vector of column names, by default `c("LRlinked", "LRnolink",
 #'   "LRnomut")`
 #'
-#' @returns The same data frame, but with an added row of total LRs.
-#' @examples
-#' res = linkedLR(paternity) |> addTotals()
+#' @returns `getTotals()` returns a named numeric. `addTotals` returns a data
+#'   frame equal to the input, but with a row of totals added at the bottom.
 #'
-#' # No effect of linkage in this case:
-#' stopifnot(all.equal(res[nrow(res), "LRlinked"],
-#'                     res[nrow(res), "LRnolink"]))
+#' @examples
+#' res = linkedLR(paternity)
+#' getTotals(res)
+#' addTotals(res)
+#'
+#' @export
+getTotals = function(x, cols = c("LRlinked", "LRnolink", "LRnomut")) {
+  cols = intersect(cols, names(x))
+  if(length(cols) == 0)
+    stop2("Columns not found")
+
+  unlist(lapply(x[cols], function(v) prod(v, na.rm = TRUE)))
+}
+
+#' @rdname getTotals
 #' @export
 addTotals = function(x, cols = c("LRlinked", "LRnolink", "LRnomut")) {
-  cols = intersect(cols, names(x))
+  tots = getTotals(x, cols)
 
   # Add totals row
   y = rbind(x, NA)
-
-  # Loop through columns
-  for(cc in cols) {
-    v = x[[cc]]
-    tot = prod(v, na.rm = TRUE)
-    y[[cc]] = c(v, tot)
-  }
-
+  y[nrow(y), names(tots)] = tots
   y[nrow(y), "Marker"] = "Total LR"
   y
 }
